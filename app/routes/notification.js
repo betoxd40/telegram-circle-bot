2327150aa3c188abed0dfbadccad8086d9f04fe8
Alberto <albertoyanesjr3@hotmail.com>
2019-08-11 12:31:26 -0300

    Test

--- app/routes/notification.js
+++ app/routes/notification.js
@@ -9,8 +9,6 @@ router.get('/', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-    console.log(req.body)
-    const { reponame, branch, build_url} = req.body.data.payload;
     try {
         //TelegramBot.sendCircleMessage(devOpsChatId, {reponame, branch, build_url})
         return res.status(200).json({
